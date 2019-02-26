f9daa8a6b1f7bbca0c754e0f896a6c8d7718ed41
José Ramón Álvarez Rojo <jsrmalvarez@gmail.com>
2019-02-26 13:03:55 +0100

    Update and rename tisp_config.h to tisp_config_example.h

--- tisp_config_example.h
+++ tisp_config_example.h
@@ -1,11 +1,5 @@
 #ifndef _TISP_CONFIG_H
 #define _TISP_CONFIG_H
-
-#ifdef DEBUG
-  #define SPAM(a) printf a
-#else
-  #define SPAM(a) (void)0
-#endif
 	
 #define MAX_ATOM_STR_SIZE 16
 #define MAX_FUN_PARAMS 5
@@ -13,8 +7,4 @@
 #define MAX_TREE_ELEMENTS 25 //256  //MAX_FUN_PARAMS^MAX_FUN_RECURSION
 #define MAX_FUN_LABEL_STR_SIZE MAX_ATOM_STR_SIZE
 
-
 #endif//_TISP_CONFIG_H_
-
-
-
